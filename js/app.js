d0e9cfff84479e0c756883d09391f9d78de7f4e0
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-18 23:47:58 +0100

    Styled the slider dots button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $('.slider').slick({
         arrows:false,
-        dots:true
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass: 'dots'
     });
 });
